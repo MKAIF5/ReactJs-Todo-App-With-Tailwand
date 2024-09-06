a13320340fd5ae1726a9ed4770aeb1b9f6a0350a
mohammed kaif <mkaif555123@gmail.com>
2024-09-06 15:48:01 +0500

    project build start

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
+      <div className="w-2/5 p-8 bg-white my-40 mx-auto rounded-2xl">
+        <h1 >Todo App</h1>
+      </div>
     </>
   );
 }
