7b13e81d77d4a85d2352ffab0b2488012bfea16a
mohammed kaif <mkaif555123@gmail.com>
2024-09-06 15:59:08 +0500

    10% Work Completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,14 @@ function App() {
   return (
     <>
       <div className="w-2/5 p-8 bg-white my-40 mx-auto rounded-2xl">
-        <h1 >Todo App</h1>
+      <div className="flex gap-3">
+        <h1 className="text-4xl">Todo App</h1>
+        <img
+          className="w-14"
+          src="https://static.wixstatic.com/media/02d54a_bd2021f8d3ac46c4a2a1061b60997db7~mv2.png/v1/crop/x_13,y_13,w_475,h_460/fill/w_440,h_428,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/booklist.png"
+          alt=""
+          />
+          </div>
       </div>
     </>
   );
