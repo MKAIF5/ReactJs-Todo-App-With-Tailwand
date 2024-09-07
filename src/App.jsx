3528ca1db1bd3f1eb1bc9f1dacd875647babb7a7
mohammed kaif <mkaif555123@gmail.com>
2024-09-08 00:50:58 +0500

    85% Work Completed

--- src/App.jsx
+++ src/App.jsx
@@ -10,34 +10,32 @@ function App() {
     setInp("");
   };
 
-  const remove = () => {
+  // const remove = () => {
 
-   };
+  // };
 
   return (
     <>
       <div className="w-2/5 p-8 bg-white my-40 mx-auto rounded-2xl">
-        <div className="flex gap-3">
+        <div className="flex items-center gap-3">
           <h1 className="text-4xl">Todo App</h1>
           <img
             className="w-14"
             src="https://static.wixstatic.com/media/02d54a_bd2021f8d3ac46c4a2a1061b60997db7~mv2.png/v1/crop/x_13,y_13,w_475,h_460/fill/w_440,h_428,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/booklist.png"
-            alt=""
+            alt="Booklist Icon"
           />
         </div>
         <br />
-        <div>
+        <div className="flex gap-1">
           <input
-            className="bg-gray-200 p-3 w-4/5 rounded-full text-black outline-none"
+            className="bg-gray-200 p-3 w-full rounded-full text-black outline-none"
             value={inp}
             placeholder="Add Your Text"
             type="text"
-            onChange={(event) => {
-              setInp(event.target.value);
-            }}
+            onChange={(event) => setInp(event.target.value)}
           />
           <button
-            className="rounded-full bg-orange-400 w-24 p-2 text-white text-lg"
+            className="rounded-3xl bg-orange-400 p-2 text-white text-lg flex-shrink-0"
             onClick={addValue}
           >
             Add
@@ -45,25 +43,23 @@ function App() {
         </div>
         <br />
         <div>
-
-        <ul className="text-gray-700 text-xl">
-          {showValue.map((items, index) => (
-            <li
-            className=" bg-slate-200 p-2 rounded-xl w-full relative"
-            key={index}
-            >
-              {items}
-              <span
-                onClick={remove}
-                className="absolute top-2 right-5 cursor-pointer hover:bg-slate-100 rounded"
+          <ul className="text-gray-700 text-xl max-h-60 overflow-auto">
+            {showValue.map((items, index) => (
+              <li
+                className="bg-slate-200 p-2 rounded-xl w-full relative mb-2"
+                key={index}
+              >
+                {items}
+                <span
+                  onClick={() => remove(index)}
+                  className="absolute top-2 right-2 cursor-pointer hover:bg-slate-100 rounded p-1"
                 >
-                &#10005;
-              </span>
-            </li>
-          ))}
-          <br />
-        </ul>
-          </div>
+                  &#10005;
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </>
   );
